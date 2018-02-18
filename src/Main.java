b9c07cb90d4b6ddac87c93547b57edd30dca35d9
Evgeniy Mityushkin <atmel13@gmail.com>
2018-02-18 14:09:55 +0200

    My Firtst commit

--- src/Main.java
+++ src/Main.java
@@ -9,14 +9,13 @@ public class Main {
      int[] arr0 = {10,20,30,40,50};
      int[] arr1 = {0,0,0,0,0,0,0,0};
 
-        System.out.println("arr0 " + Arrays.toString(arr0));
+        System.out.println("arr0rr " + Arrays.toString(arr0));
         System.out.println("arr1 " + Arrays.toString(arr1));
 
         System.arraycopy(arr0, 1, arr1,2,3);
         System.out.println("arr0: " + Arrays.toString(arr1));
 
-
-
+zxcsdc123
     }
 
 }
